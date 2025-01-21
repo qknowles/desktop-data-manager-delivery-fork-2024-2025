2377dfac235267d963b05c6ff204ab9b86269fc5
qknowles <qknowles@asu.edu>
2025-01-21 14:09:41 -0600

    Add adjustable column widths functionality.

--- src/components/TableHeading.jsx
+++ src/components/TableHeading.jsx
@@ -1,5 +1,7 @@
 import classNames from 'classnames';
 import { SortAscIcon, SortDescIcon } from '../assets/icons';
+import '../styles/Table.css';
+import React from 'react';
 
 export const TableHeading = ({ label, active, sortDirection, onClick }) => {
 
@@ -10,15 +12,15 @@ export const TableHeading = ({ label, active, sortDirection, onClick }) => {
     };
 
     const thClasses = classNames(
-        'sticky top-0 bg-white z-10 border-b border-neutral-800 dark:bg-neutral-950 p-2 font-semibold cursor-pointer',
+        'sticky top-0 bg-white z-10 border-b border-neutral-800 dark:bg-neutral-950 p-1 font-semibold cursor-pointer',
         { 'text-asu-maroon dark:text-asu-gold': active }
     );
 
     return (
         <th className={thClasses} onClick={onClick}>
-            <div className="flex items-center justify-center">
+            <div className="flex items-center justify-start pl-2">
                 <span className="flex-1 mr-1 whitespace-nowrap">{label}</span>
-                <span className="flex-4 text-xl">{getSortIcon()}</span>
+                <span className="flex-4 text-xl ml-1">{getSortIcon()}</span>
             </div>
         </th>
     );
