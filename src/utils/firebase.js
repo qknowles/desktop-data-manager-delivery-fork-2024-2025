01b7f05da3e4610fb5d7a485ffca5d15965358e9
qknowles <qknowles@asu.edu>
2025-01-30 13:49:42 -0600

    clear firebase cache

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,7 +1,7 @@
 // Firebase import
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, clearIndexedDbPersistence } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 // SKD's that are available:
@@ -23,3 +23,7 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+
+clearIndexedDbPersistence(db).then(() => {
+    console.log("Firestore cache cleared");
+});
